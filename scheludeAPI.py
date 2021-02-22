a227bbcd55e70942795cb47a88f22c8de8b236aa
yaSeryoga <sk2sk@outlook.com>
2021-02-22 21:15:55 +0300

    delete tabs

--- scheludeAPI.py
+++ scheludeAPI.py
@@ -5,24 +5,22 @@ from bs4 import BeautifulSoup
 def getGroupScheludeURL(group):
     if group[:3] == '353':
         url = 'https://ruz.spbstu.ru/faculty/95/groups'
-        response = requests.get(url)
-        #print(response.text)  #return html
-        # for str in response.text.find_all('a'):
-        #     if group in str:
-        #         print(str)
-
-        soup = BeautifulSoup(response.content, 'html.parser')
-        links = soup.find_all('a')
-        #print(links)
-
-        for link in links:
-            if group in link:
-                url = 'https://ruz.spbstu.ru/' + link.get('href')
-
-                
-        #print(bufStr)
-        #url = bufStr.get('href')
-        print(url)
+        
+    response = requests.get(url)
+    #print(response.text)  #return html
+    # for str in response.text.find_all('a'):
+    #     if group in str:
+    #         print(str)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    links = soup.find_all('a')
+    #print(links)
+    for link in links:
+        if group in link:
+            finalurl = 'https://ruz.spbstu.ru/' + link.get('href')
+            
+    #print(bufStr)
+    #url = bufStr.get('href')
+    print(finalurl)
 
 
 
