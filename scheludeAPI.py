8c6d6217cb303cdb98141e678f144b65e626018a
yaSeryoga <sk2sk@outlook.com>
2021-02-24 21:48:33 +0300

    код 200, инфобез бакалавры

--- scheludeAPI.py
+++ scheludeAPI.py
@@ -1,44 +1,68 @@
 import requests
 from bs4 import BeautifulSoup
+#from selenium import webdriver
+
+import datetime
 
 
-def getGroupScheludeURL(group):
-    if group[:3] == '353':
+def _getGroupScheludeURL(group):
+    if group[:3] == '353':  #ИКНТ бакалавры
         url = 'https://ruz.spbstu.ru/faculty/95/groups'
-        
-    response = requests.get(url)
-    #print(response.text)  #return html
-    # for str in response.text.find_all('a'):
-    #     if group in str:
-    #         print(str)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    links = soup.find_all('a')
-    #print(links)
-    for link in links:
-        if group in link:
-            finalurl = 'https://ruz.spbstu.ru/' + link.get('href')
+        response = requests.get(url)
+    elif group[:3] == '354':  #ИКНТ магистры
+        url = 'https://ruz.spbstu.ru/faculty/95/groups'
+        response = requests.get(url)
+        #как нажать на кнопку "магистр??"     
+
+    elif group[:3] == '483':
+        url = 'https://ruz.spbstu.ru/faculty/122/groups'
+        response = requests.get(url)
+    
+    
+    if response.status_code == 200:
+
+        soup = BeautifulSoup(response.content, 'html.parser')
+        links = soup.find_all('a')
+        for link in links:
+            if group in link:
+                finalurl = 'https://ruz.spbstu.ru/' + link.get('href')
             
-    #print(bufStr)
-    #url = bufStr.get('href')
-    print(finalurl)
+        return finalurl
+    else:
+        return 'САЙТ ЛЕЖИТ'
 
 
 
 
-def todaySchecude(group):
-    print('buf')
+def todaySchecude(group, lang):
+    now = datetime.datetime.now()
+    print(now.month, now.hour, now.minute)
+    print('today')
+    url = _getGroupScheludeURL(group)
+    response = requests.get(url)
+    #print(response.text)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    days = soup.find_all('li', {'class': 'schedule__day'})
+    # for day in days:
+    #     for line in lines:
+    #         if  
+    
+    
 
 
-def tomottowSchelude(group):
-    print('buf')
+
+def tomottowSchelude(group, lang):
+    print('tomorrow')
 
 
-def thisWeekSchelude(group):
-    print('buf')
+def thisWeekSchelude(group, lang):
+    print('this week')
 
 
-def nextWeekSchelude(group):
-    print('buf') 
+def nextWeekSchelude(group, lang):
+    print('next week') 
 
-
-getGroupScheludeURL('3532703/90001')
+#3540202/00201
+#3532703/90001
+print(_getGroupScheludeURL('4831001/00001'))
+#todaySchecude('3532703/90001', 'RU')
